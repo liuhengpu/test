1c791f8392165281f644dd680cb55c1deb15f433
kingo33 <hengliu1314@sina.com>
2019-01-21 13:28:30 +0800

    liu

--- app/src/main/java/com/shici/kingo33/testdemoshichi/MainActivity.java
+++ app/src/main/java/com/shici/kingo33/testdemoshichi/MainActivity.java
@@ -2,6 +2,7 @@ package com.shici.kingo33.testdemoshichi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ceishi
+        TextView tv = new TextView(this);
+
+        tv.setText("liuhengpu");
     }
 }
