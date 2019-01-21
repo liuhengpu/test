ed5ea73cd26c83380d9d587a2e4635bfba4afd4f
kingo33 <hengliu1314@sina.com>
2019-01-21 13:37:17 +0800

    yy

--- app/src/main/java/com/shici/kingo33/testdemoshichi/MainActivity.java
+++ app/src/main/java/com/shici/kingo33/testdemoshichi/MainActivity.java
@@ -1,16 +1,19 @@
 package com.shici.kingo33.testdemoshichi;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = new TextView(this);
-        tv.setText("liuhengpu");
+
     }
 }
